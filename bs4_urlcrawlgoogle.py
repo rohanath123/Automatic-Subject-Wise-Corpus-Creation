f5c670df9356ee16ce83cbdb954eda146e6a3cdf
Rohan Athawade <39797571+rohanath123@users.noreply.github.com>
2020-02-01 20:12:02 +0530

    Clean code, strip tags, create list of urls retrieved as strings

--- bs4_urlcrawlgoogle.py
+++ bs4_urlcrawlgoogle.py
@@ -2,29 +2,19 @@ import bs4 as bs
 import urllib.request
 import requests
 
-keyword = "dynamic programming"
+keyword = "asap rocky"
 keyword = keyword.replace(" ", "+")
 source = requests.get("https://www.google.dz/search?q=" + keyword)
 soup = bs.BeautifulSoup(source.content)
 
 links = soup.findAll("a")
 
-
+urls = []
 for url in links:
 	if "/url?q=" in str(url.get('href')):
-		print(url.get('href'))
+		urls.append(str(url.get('href')).strip("/url?q="))
 
 
-
-
-
-'''
-import requests
-from bs4 import BeautifulSoup
-page = requests.get("https://www.google.dz/search?q=see", 'html5lib')
-soup = BeautifulSoup(page.content)
-links = soup.findAll("a")
-
-print(links[0])'''
+print(urls)
 
 
