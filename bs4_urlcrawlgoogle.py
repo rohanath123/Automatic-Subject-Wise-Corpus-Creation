1f7427ea40c0f8c59db33f14bc0a7c333d2dc4ae
Rohan Athawade <39797571+rohanath123@users.noreply.github.com>
2020-02-02 14:31:55 +0530

    Put everything in functions
    Put all code in functions so that other scripts can import and call the functions to use, for example in content cleaning.

--- bs4_urlcrawlgoogle.py
+++ bs4_urlcrawlgoogle.py
@@ -3,7 +3,7 @@ import urllib.request
 import requests
 from pprint import pprint
 
-
+'''
 keyword = "Cloud Computing"
 keyword = keyword.replace(" ", "+")
 
@@ -40,4 +40,47 @@ with open("C:/Users/Rohan/Desktop/Tests/"+keyword+".txt", 'a', encoding = 'utf-8
 	for para in content:
 		f.write(str(para))
 
-print("Finished Writing.")
+print("Finished Writing.")'''
+
+
+def get_content_from_keyword(keyword):
+	keyword = keyword.replace(" ", "+")
+	link = "https://www.google.dz/search?q=" + keyword
+	source = requests.get(link)
+	soup = bs.BeautifulSoup(source.content, 'lxml')
+
+	links = soup.findAll("a")
+
+	urls = []
+	for url in links:
+		if "/url?q=" in str(url.get('href')):
+			urls.append(str(url.get('href')).strip("/url?q="))
+
+	urls_clean = [str(urls[i])[0:str(urls[i]).find("&sa")] for i in range(len(urls))]
+	urls_clean.pop(len(urls_clean)-1)
+
+	print("Links Found: " + str(len(urls_clean)))
+
+	content = []
+	i=0
+	for url in urls_clean:
+		i = i+1
+		try:
+			source2 = urllib.request.urlopen(url).read()
+			soup = bs.BeautifulSoup(source2, 'lxml')
+			for paragraph in soup.find_all('p'):
+				content.append(str(paragraph.text).strip())
+		except:
+			print("Link Number: "+str(i)+" is Forbidden.")
+
+	return content
+
+def write_to_txt(content, keyword):
+	#keyword = keyword.replace("+", " ")
+	with open("C:/Users/Rohan/Desktop/Tests/"+keyword+".txt", 'a', encoding = 'utf-8') as f:
+		for para in content:
+			f.write(str(para))
+
+	print("Finished Writing.")
+
+
