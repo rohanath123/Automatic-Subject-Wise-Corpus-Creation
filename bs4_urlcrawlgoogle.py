c3c4b3b9094d8c1a533ceff8a21cdd83ab4bb7f9
Rohan Athawade <39797571+rohanath123@users.noreply.github.com>
2020-02-06 00:32:37 +0530

    Added string url column in content
    Added url from which content was scraped next to each row of content to be able to search content with respect to a given website.

--- bs4_urlcrawlgoogle.py
+++ bs4_urlcrawlgoogle.py
@@ -3,6 +3,46 @@ import urllib.request
 import requests
 from pprint import pprint
 
+'''
+keyword = "Cloud Computing"
+keyword = keyword.replace(" ", "+")
+
+link = "https://www.google.dz/search?q=" + keyword
+source = requests.get(link)
+soup = bs.BeautifulSoup(source.content, 'lxml')
+
+links = soup.findAll("a")
+
+urls = []
+for url in links:
+	if "/url?q=" in str(url.get('href')):
+		urls.append(str(url.get('href')).strip("/url?q="))
+
+urls_clean = [str(urls[i])[0:str(urls[i]).find("&sa")] for i in range(len(urls))]
+urls_clean.pop(len(urls_clean)-1)
+
+print("Links Found: " + str(len(urls_clean)))
+
+content = []
+i=0
+for url in urls_clean:
+	i = i+1
+	try:
+		source2 = urllib.request.urlopen(url).read()
+		soup = bs.BeautifulSoup(source2, 'lxml')
+		for paragraph in soup.find_all('p'):
+			content.append(str(paragraph.text).strip())
+	except:
+		print("Link Number: "+str(i)+" is Forbidden")
+
+keyword = keyword.replace("+", " ")
+with open("C:/Users/Rohan/Desktop/Tests/"+keyword+".txt", 'a', encoding = 'utf-8') as f:
+	for para in content:
+		f.write(str(para))
+
+print("Finished Writing.")'''
+
+
 def get_content_from_keyword(keyword):
 	keyword = keyword.replace(" ", "+")
 	link = "https://www.google.dz/search?q=" + keyword
@@ -30,7 +70,7 @@ def get_content_from_keyword(keyword):
 			source2 = urllib.request.urlopen(url).read()
 			soup = bs.BeautifulSoup(source2, 'lxml')
 			for paragraph in soup.find_all('p'):
-				content.append(str(paragraph.text).strip())
+				content.append([str(paragraph.text).strip(), str(url)])
 		except:
 			print("Link Number: "+str(i)+" is Forbidden.")
 
@@ -38,7 +78,7 @@ def get_content_from_keyword(keyword):
 
 def write_to_txt(content, keyword):
 	#keyword = keyword.replace("+", " ")
-	with open("C:/Users/Rohan/Desktop/Tests/"+keyword+".txt", 'a', encoding = 'utf-8') as f:
+	with open("C:/Users/Rohan/Desktop/keyword Results/"+keyword+".txt", 'a', encoding = 'utf-8') as f:
 		for para in content:
 			f.write(str(para))
 
